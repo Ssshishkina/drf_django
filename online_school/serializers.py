350ba543e20a43262fcd3bfc80b8f414927fb9a2
Ssshishkina <ana.koze@yandex.ru>
2024-05-19 00:56:24 +0400

    в сериализатор добавлено поле вывода кол-ва уроков; добавлена модель "платежи", описаны поля

--- online_school/serializers.py
+++ online_school/serializers.py
@@ -3,6 +3,11 @@ from online_school.models import Course, Lesson
 
 
 class CourseSerializer(serializers.ModelSerializer):
+    lessons_count = serializers.SerializerMethodField(default=0)
+
+    def get_lessons_count(self, instance):
+        return instance.lesson.count()
+
     class Meta:
         model = Course
         fields = '__all__'
--- users/models.py
+++ users/models.py
@@ -20,3 +20,26 @@ class User(AbstractUser):
 
     def __str__(self):
         return self.email
+
+
+class Payments(models.Model):
+
+    PAYMENT_METHOD = [('cash', 'Наличные'), ('transfer', 'Перевод на счет')]
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='Пользователь',
+                             related_name='payments')
+    payment_date = models.DateTimeField(auto_now=True, verbose_name='Дата оплаты')
+    course_paid = models.ForeignKey('lms.Course', on_delete=models.CASCADE, verbose_name='Оплаченный курс',
+                                    related_name='payments')
+    lesson_paid = models.ForeignKey('lms.Lesson', on_delete=models.CASCADE, verbose_name='Оплаченный урок',
+                                    related_name='payments')
+    amount = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='Сумма оплаты')
+    payment_method = models.CharField(max_length=100, verbose_name='Способ оплаты')
+
+    def __str__(self):
+        return f'Оплата от {self.user} в размере {self.amount}'
+
+    class Meta:
+        verbose_name = 'Платеж'
+        verbose_name_plural = 'Платежи'
+        ordering = ('user', 'payment_date',)
